ec94bee47292fc46db396e988be0542ab877232f
miclp <michaelpeimani@gmail.com>
2016-06-29 14:41:03 -0700

    Add edit capabilities to figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -78,10 +78,58 @@ class FiguresController < ApplicationController
     erb :'/figures/edit'
   end
 
-  patch '/songs/:id/edit' do
+  patch '/figures/:id/edit' do
     # update the song and save it
+    @figure = Figure.find(params[:id])
+    # binding.pry
+
+    # Update the figure name
+    @figure.name = params[:figure][:name]
+
+    # compile all the landmarks
+    landmarks = []
+    if params[:figure].key?("landmark_ids")  # check if it's not nil
+      if !params[:figure][:landmark_ids].empty?
+        params[:figure][:landmark_ids].each do |landmark_id|
+          landmarks << Landmark.find(landmark_id)
+        end
+      end
+    end
+
+    if !params[:landmark][:name].empty?  # it will exist as "" but might be empty
+      landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+      if !landmarks.include?(landmark)
+        landmarks << landmark
+      end
+    end
+    @figure.landmarks = landmarks
+
+    #### Titles ####
+    # compile all the titles
+    titles = []
+    if params[:figure].key?("title_ids")  # check if it's not nil
+      if !params[:figure][:title_ids].empty?
+        params[:figure][:title_ids].each do |title_id|
+          titles << Title.find(title_id)
+        end
+      end
+    end
+
+    if !params[:title][:name].empty?  # it will exist as "" but might be empty
+      title = Title.find_or_create_by(name: params[:title][:name])
+      if !titles.include?(title)
+        titles << title
+      end
+    end
+    @figure.titles = titles
+
+    #### End Titles ####
+    @figure.save
+
+
 
     # redirect to the song's show page
+    redirect "/figures/#{@figure.id}"
   end
 
 
